143a3e46e4d7f3d95b8701aa4f9fd15c9b9c002b
skowalskidev <skowalskidev@gmail.com>
2024-04-06 14:55:07 +0700

    fix redundant scroll

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Table from "@/components/Table"
 export default function Home() {
   return (
     <StoreProvider>
-      <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <main className="flex min-h-full flex-col items-center justify-between p-6">
         <Table />
       </main>
     </StoreProvider>
