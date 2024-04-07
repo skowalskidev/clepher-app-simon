540055ca10a7a38df24ab99de429eeac574cc570
skowalskidev <skowalskidev@gmail.com>
2024-04-07 19:17:51 +0700

    add link to button

--- components/Actions/SquareButton.tsx
+++ components/Actions/SquareButton.tsx
@@ -1,12 +1,18 @@
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
+
 interface Props {
     children: React.ReactNode
     active?: boolean;
+    href?: Url;
 }
 
-export const SquareButton = ({ children, active }: Props) => {
+export const SquareButton = ({ children, active, href }: Props) => {
     return (
-        <button className={`btn btn-square ${active ? 'bg-[#031531] text-[#c5cbd3] hover:bg-[#031531]' : ''}`}>
-            {children}
-        </button>
+        <Link href={href ? href : ''}>
+            <button className={`btn btn-square ${active ? 'bg-[#031531] text-[#c5cbd3] hover:bg-[#031531]' : ''}`}>
+                {children}
+            </button>
+        </Link>
     )
 }
--- components/Page/Sidebar.tsx
+++ components/Page/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Horseshoe } from "../Icons/Horseshoe"
 export const Sidebar = () => {
     return (
         <nav className="hidden sm:block fixed menu border-r border-r-base-300 min-h-full bg-base-100 mt-16">
-            <SquareButton active>
+            <SquareButton href="/" active>
                 <Horseshoe />
             </SquareButton>
         </nav>
